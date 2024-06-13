4f1800538f87236670b3f6a16f4b86bc515aa376
arman <armbak86@gmail.com>
2024-06-13 23:30:00 +0330

    Set up project base.

---
+++ Diet.WebMVC/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore;
+global using Diet.WebMVC.Models;
+global using Microsoft.AspNetCore.Mvc;
+global using System.Diagnostics;
+global using Diet.Domain.Common;
+global using Diet.WebMVC.Infrastructure;
+global using Microsoft.AspNetCore.Identity;
---
+++ Diet.WebMVC/Infrastructure/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿namespace Diet.WebMVC.Infrastructure;
+
+public class AppDbContext : IdentityDbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+}
