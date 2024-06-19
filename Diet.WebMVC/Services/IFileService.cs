e01cdc230d5974db33ce35821bf885fb53ed6b3d
arman <armbak86@gmail.com>
2024-06-19 19:48:35 +0330

    Add file service for handling file operations.

---
+++ Diet.WebMVC/Services/IFileService.cs
@@ -0,0 +1,7 @@
+﻿namespace Diet.WebMVC.Services;
+
+public interface IFileService
+{
+    Task CreateFile(IFormFile file, string dest, string fileName);
+    void DeleteFile(string directory, string fileName);
+}
